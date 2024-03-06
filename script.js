7cc553d6f08326a26167dfa1020011f81f2e03dd
Nikhil <longestwavelength@protonmail.com>
2024-03-06 15:40:35 +0530

    some not so required changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const password = document.getElementById('password')
 const background = document.getElementById('background')
+const toggleButton = document.getElementById('togglePassword');
+const passwordIcon = toggleButton.querySelector('i');
 
 password.addEventListener('input', (e) => {
     const input = e.target.value
@@ -7,3 +9,17 @@ password.addEventListener('input', (e) => {
     const blurValue = 20 - length * 2
     background.style.filter = `blur(${blurValue}px)`
 })
+
+toggleButton.addEventListener('click', function() {
+    if (password.type === 'password') {
+        password.type = 'text';
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+    } else {
+        password.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+    }
+});
+// also not a good way to store the password in plain text. they should
+// always be stored on the server side
